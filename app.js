460bffc467a17ec1e5999fcd5ab3117dc8ed693b
Aaron Hampson <aaron0819@users.noreply.github.com>
2016-06-15 11:55:21 -0400

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ var trips;
 function printTrips() {
   var tripIds = "";
 
-  for (var i trips.length - 1; i >= 0; i--) {
+  for (var i = trips.length - 1; i >= 0; i--) {
     tripIds += trips[i];
   }
 
