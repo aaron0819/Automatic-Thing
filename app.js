7537d0a115d7cb4f878a565598016bff731e2e5d
Aaron Hampson <aaron0819@users.noreply.github.com>
2016-06-15 23:54:09 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -127,31 +127,13 @@ app.get('/claims', function(req, res) {
   console.log("/claims");
 
   request.get({
-    uri: "https://api.automatic.com/user/me",
+    uri: "https://api.automatic.com/user/me/",
     headers: {Authorization: 'Bearer ' + req.session.token.token.access_token},
     json: true
   }, function(e, r, body) {
     if(e){
     } else{
       user = body;
-
-      request.get({
-        uri: "https://api.automatic.com/vehicle/",
-        headers: {Authorization: 'Bearer ' + req.session.token.token.access_token},
-        json: true
-      }, function(e, r, body) {
-        if(e){
-        } else{
-          vehicle = body.results[0];
-
-          res.render('claims', {
-            trips: trips,
-            vehicle: vehicle,
-            user: user;
-          });
-        }
-      });
-
     }
   });
 });
