a2b79001ca8b7a04e4fbf12df24c5565fa503b76
Aaron Hampson <aaron0819@users.noreply.github.com>
2016-06-16 05:57:08 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const request = require('request');
 const express = require('express');
 const session = require('express-session');
@@ -21,10 +22,44 @@ const authorizationUri = oauth2.authCode.authorizeURL({
   scope: 'scope:user:profile scope:trip scope:location scope:vehicle:profile scope:vehicle:events scope:behavior'
 });
 
+var nodemailer = require('nodemailer');
+var router = express.Router();
+app.use('/sendMail', router);
+router.post('/', handleSendMail);
+
 var api = require('./api');
 var trips;
 var vehicle;
 var user;
+
+function handleSayHello(req, res) {
+    // Not the movie transporter!
+    var transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: 'aaron.d.hampson@gmail.com', // Your email id
+            pass: 'password987' // Your password
+        }
+    });
+
+    var mailOptions = {
+	    from: 'aaron.d.hampson@gmail.com>', // sender address
+	    to: 'aaron.d.hampson@gmail.com', // list of receivers
+	    subject: 'Email Example', // Subject line
+	    text: text //, // plaintext body
+	    // html: '<b>Hello world ✔</b>' // You can choose to send an HTML body instead
+	};
+
+	transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+        console.log(error);
+        res.json({yo: 'error'});
+    }else{
+        console.log('Message sent: ' + info.response);
+        res.json({yo: info.response});
+    };
+});
+}
 
 function printTrips() {
   var tripIds = "";
