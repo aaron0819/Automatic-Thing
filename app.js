a4e76bb39e471a886aa08b409a925a1e8557a894
Aaron Hampson <aaron0819@users.noreply.github.com>
2016-06-16 05:29:22 -0300

    Update app.js

--- app.js
+++ app.js
@@ -268,7 +268,7 @@ app.get('/claims4', function(req, res) {
 app.get('/dashboard', function(req, res) {
   console.log("/dashboard");
 
-  res.render('dashboard', {
+  res.render('dashboard-new', {
   });
 });
 
